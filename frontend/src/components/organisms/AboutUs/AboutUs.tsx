ae776c295c6be31655580c381bf718ee900c338f
drumil1602 <drumil.akhenia@pirimidtech.com>
2024-05-24 01:21:04 +0530

    removed extra text

--- frontend/src/components/organisms/AboutUs/AboutUs.tsx
+++ frontend/src/components/organisms/AboutUs/AboutUs.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import instructorListData from '../../../data/teamInfo.json';
-import { Instructor } from '../../../types/types';
+// import instructorListData from '../../../data/teamInfo.json';
+// import { Instructor } from '../../../types/types';
 // import Card from '../../molecule/FeaturesCard/Card';
 
 const AboutUs: React.FC = () => {
     return (
-        <div>
-            <p>The Team</p>
-            <p>Fueled with passion for learning</p>
-            {
-                instructorListData.map((personInfo: Instructor) => (
-                    <div>
-                        <p>
-                            {personInfo.name}
-                        </p>
-                        <p>
-                            {personInfo.designation}
-                        </p>
-                        <p>
-                            {personInfo.college}
-                        </p>
-                        <p>
-                            {personInfo.profilePicture}{personInfo.instagram}{personInfo.linkedin}
-                        </p>
-                    </div>
-                ))
-            }
-        </div>
+        <></>
+        // <div>
+        //     <p>The Team</p>
+        //     <p>Fueled with passion for learning</p>
+        //     {
+        //         instructorListData.map((personInfo: Instructor) => (
+        //             <div>
+        //                 <p>
+        //                     {personInfo.name}
+        //                 </p>
+        //                 <p>
+        //                     {personInfo.designation}
+        //                 </p>
+        //                 <p>
+        //                     {personInfo.college}
+        //                 </p>
+        //                 <p>
+        //                     {personInfo.profilePicture}{personInfo.instagram}{personInfo.linkedin}
+        //                 </p>
+        //             </div>
+        //         ))
+        //     }
+        // </div>
     );
 }
 
