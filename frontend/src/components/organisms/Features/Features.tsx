20c4f780aee7f11773090c2f62c337868f65cba5
drumil1602 <drumil.akhenia@pirimidtech.com>
2024-05-24 17:22:21 +0530

    fix : typo

--- frontend/src/components/organisms/Features/Features.tsx
+++ frontend/src/components/organisms/Features/Features.tsx
@@ -6,7 +6,7 @@ const Features: React.FC = () => {
   return (
     <section className={styles.featureSection}>
       <div className={styles.featuresContainer}>
-        <h1 className={styles.sectionTitle}>Our programme is curated to maximise real-world learning</h1>
+        <h1 className={styles.sectionTitle}>Our programe is curated to maximise real-world learning</h1>
         <FeaturesCarausal />
       </div>
     </section>
