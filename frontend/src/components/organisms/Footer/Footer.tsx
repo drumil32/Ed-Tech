4e875757d6582f92b54e11572394a58bf0074bce
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-06-14 18:56:38 +0530

    footer updated

--- frontend/src/components/organisms/Footer/Footer.tsx
+++ frontend/src/components/organisms/Footer/Footer.tsx
@@ -8,6 +8,27 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <div className="footer">
+        <div className="credits">
+          <h2>Built by students of the same program</h2>
+          <div className="developerImageContainer">
+            <a
+              href="https://www.linkedin.com/in/mayank-gupta-752328173/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="developer mayank"
+            >
+              <img src="/assests/developer/mayank.jpg" alt="mayank" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/drumil-akhenia/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="developer drumil"
+            >
+              <img src="/assests/developer/drumil.jpg" alt="drumil" />
+            </a>
+          </div>
+        </div>
         <div className="information">
           <ContactInfo />
           <div className="footer_rightSection">
@@ -15,13 +36,19 @@ const Footer: React.FC = () => {
             <div className="another-navigation">
               <ul>
                 <li>
-                  <Link style={{ color: "white" }} to="/faqs" >FAQs</Link>
+                  <Link style={{ color: "white" }} to="/faqs">
+                    FAQs
+                  </Link>
                 </li>
                 <li>
-                  <Link style={{ color: "white" }} to="/faqs" >Terms of Use</Link>
+                  <Link style={{ color: "white" }} to="/faqs">
+                    Terms of Use
+                  </Link>
                 </li>
                 <li>
-                  <Link style={{ color: "white" }} to="/faqs" >Privacy Policy</Link>
+                  <Link style={{ color: "white" }} to="/faqs">
+                    Privacy Policy
+                  </Link>
                 </li>
               </ul>
             </div>
