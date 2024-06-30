a2ba273c4b12214a24be91ba9b6a86f0ee2cf685
drumil1602 <drumil.akhenia@pirimidtech.com>
2024-06-30 22:36:31 +0530

    refeact : frontend

--- frontend/src/components/organisms/BookLiveClass/BookLiveClass.tsx
+++ frontend/src/components/organisms/BookLiveClass/BookLiveClass.tsx
@@ -9,6 +9,7 @@ import demoSessionSchedule from "../../../data/demoSessionSchedule.json";
 import { toast } from 'react-toastify';
 import { useFormContext } from "../../../context/formContext";
 import restEndPoints from "../../../data/restEndPoints.json";
+import { validateName, validatePhoneNumber } from "../../../utils/validations";
 
 export interface ProfileData {
   image: string;
@@ -32,26 +33,7 @@ const BookLiveClassForm: React.FC = () => {
   const [numberError, setNumberError] = useState<string | null>(null);
   const { isLoading, setLoading, formSubmitted, setFormSubmitted } = useFormContext();
 
-  const validateName = (value: string): string | null => {
-    const namePattern = /^[A-Za-z\s]+$/;
-    if (value.trim().length < 3) {
-      return "Name must be at least 3 characters long.";
-    }
-    if (!namePattern.test(value)) {
-      return "Name should only contain alphabets and spaces.";
-    }
-    if (value.trim().length > 50) {
-      return "Name should not have more than 50 characters.";
-    }
-    return null;
-  };
-  const validatePhoneNumber = (value: string): string | null => {
-    const phoneNumberPattern = /^[0-9]{10}$/;
-    if (!phoneNumberPattern.test(value)) {
-      return "Please enter a valid 10-digit mobile number.";
-    }
-    return null;
-  };
+  
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
--- frontend/src/pages/ContactUs/ContactUs.tsx
+++ frontend/src/pages/ContactUs/ContactUs.tsx
@@ -11,6 +11,7 @@ import axios from "axios";
 import contactDetails from "../.../../../data/contactDetails.json";
 import { toast } from "react-toastify";
 import restEndPoints from "../../data/restEndPoints.json";
+import { validateName, validatePhoneNumber } from "../../utils/validations";
 
 const ContactUs: React.FC = () => {
   const [inputName, setInputName] = useState<string>("");
@@ -36,26 +37,7 @@ const ContactUs: React.FC = () => {
       link: `mailto:${contactDetails.email}`
     },
   ];
-  const validateName = (value: string): string | null => {
-    const namePattern = /^[A-Za-z\s]+$/;
-    if (value.trim().length < 3) {
-      return "Name must be at least 3 characters long.";
-    }
-    if (!namePattern.test(value)) {
-      return "Name can only contain alphabets and spaces.";
-    }
-    if (value.trim().length > 50) {
-      return "Name can not have more than 50 characters.";
-    }
-    return null;
-  };
-  const validatePhoneNumber = (value: string): string | null => {
-    const phoneNumberPattern = /^[0-9]{10}$/;
-    if (!phoneNumberPattern.test(value)) {
-      return "Please enter a valid 10-digit mobile number.";
-    }
-    return null;
-  };
+
   const validateMessage = (value: string): string | null => {
     if (value.trim().length > 200) {
       return "Message can not have more than 200 characters.";
