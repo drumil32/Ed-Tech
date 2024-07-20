58e0eb641e79903bd00be0d78eb622659b61af95
drumil32 <akheniad@gmail.com>
2024-07-20 18:11:49 +0530

    feat : dummy course is added

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import liveClassBookingRoutes from './liveClassBookingRoutes.js';
 import requestACallRoutes from './requestACallRoutes.js';
 import enrollStudentRoutes from './enrollStudentRoutes.js';
 import counsellingSessionRoutes from './counsellingSessionRoutes.js';
+import courseRoutes from './courseRoutes.js';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/enroll', enrollStudentRoutes)
 router.use('/book-live-class', liveClassBookingRoutes);
 router.use('/request-a-call', requestACallRoutes);
 router.use('/counselling-session', counsellingSessionRoutes);
+router.use('/course', courseRoutes)
 
 export default router;
